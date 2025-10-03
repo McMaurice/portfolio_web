736c1ee50f50b833097da621da2188d8726d4cb3
McMaurice <macmauriceosuji@gmail.com>
2025-10-03 09:30:37 +0100

    Fix base href for GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "4f5d04c18e2ef40870df07f6305486e8",
+const RESOURCES = {"flutter_bootstrap.js": "528683bf5184538f9bfe22f090cab0a2",
 "version.json": "b6c2da712505c7c8b85d33ea50fdccce",
-"index.html": "3d489c7ea7c6f28b15d9fe1c13b7ec52",
-"/": "3d489c7ea7c6f28b15d9fe1c13b7ec52",
+"index.html": "abbcc2a67db937cc323668cfc63ffe1f",
+"/": "abbcc2a67db937cc323668cfc63ffe1f",
 "main.dart.js": "fb18a0676c520082900a8bc68ac66e91",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
